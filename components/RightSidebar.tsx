70a07927e9e55d8c09f598d88b8286c519e35cf4
tringuyen@motive <95064327+tringuyenCoding@users.noreply.github.com>
2024-11-06 21:19:08 +0700

    Credit card on right side bar

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,4 +1,7 @@
+import Link from 'next/link'
+import Image from 'next/image'
 import React from 'react'
+import BankCard from '@/components/BankCard'
 
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
   return (
@@ -24,7 +27,48 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
         </div>
       </section>
 
+      <section className='banks'>
+        <div className='flex w-full justify-between'>
+          <h2 className='header-2'>My Banks</h2>
+          <Link
+            href='/'
+            className='flex gap-2'>
+            <Image
+              src="/icons/plus.svg"
+              width={20}
+              height={20}
+              alt='plus'>
+            </Image>
+            <h2 className='text-14 font-semibold text-gray-600'>
+              Add Bank
+            </h2>
+          </Link>
+        </div>
 
+        {banks?.length > 0 && (
+          <div className='relative flex flex-1 flex-col items-center justify-center
+          gap-5'>
+            <div className='relative z-10'>
+              <BankCard
+                key={banks[0].$id}
+                account={banks[0]}
+                userName={`${user.firstName} ${user.lastName}`}
+                showBalance={false}
+              />
+            </div>
+            {banks[1] && (
+              <div className='absolute right-0 top-8 z-0 w-[90%]'>
+                <BankCard
+                  key={banks[1].$id}
+                  account={banks[1]}
+                  userName={`${user.firstName} ${user.lastName}`}
+                  showBalance={false}
+                />
+              </div>
+            )}
+          </div>
+        )}
+      </section>
     </aside>
   )
 }
