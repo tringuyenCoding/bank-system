4b878656abbce68637ae0247d11e66c3c5ca8410
tringuyen@motive <95064327+tringuyenCoding@users.noreply.github.com>
2024-11-08 11:05:20 +0700

    Complete the layout of sign in and sign up

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,20 +18,22 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { Divide } from 'lucide-react'
-
-const formSchema = z.object({
-  email: z.string().email(),
-})
+import { Divide, Loader2 } from 'lucide-react'
+import CustomInput from './CustomInput'
+import { authFormSchema } from '@/lib/utils'
 
 const AuthForm = ({ type }: { type: string }) => {
   const [user, setUser] = useState(null)
+  const [isLoading, setLoading] = useState(false)
+
+  const formSchema = authFormSchema(type)
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      email: "",
+      email: '',
+      password: '',
     },
   })
 
@@ -39,7 +41,9 @@ const AuthForm = ({ type }: { type: string }) => {
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
+    setLoading(true)
     console.log(values)
+    setLoading(false)
   }
 
   return (
@@ -70,62 +74,69 @@ const AuthForm = ({ type }: { type: string }) => {
             {/* PlaidLnk */}
           </div>
         ) : (
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <div className='form-item'>
-                    <FormLabel className='form-label'>
-                      Email
-                    </FormLabel>
-                    <div className='flex w-full flex-col'>
-                      <FormControl>
-                        <Input placeholder='Enter your email'
-                          className='input-class' {...field}>
+          <>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8">
+                {
+                  type === 'sign-up' && (
+                    <>
+                      <div className='flex gap-4'>
+                        <CustomInput control={form.control} name='firstName'
+                          label='First Name' placeholder='Enter your first name' />
+                        <CustomInput control={form.control} name='lastName'
+                          label='Last Name' placeholder='Ex: Doe' />
+                      </div>
+                      <CustomInput control={form.control} name='address1'
+                        label='Address' placeholder='Enter your specific address' />
+                      <div className='flex gap-4'>
+                        <CustomInput control={form.control} name='state'
+                          label='Sate' placeholder='Ex: NY' />
+                        <CustomInput control={form.control} name='postalCode'
+                          label='Postal Code' placeholder='ex: 11101' />
+                      </div>
+                      <div className='flex gap-4'>
+                        <CustomInput control={form.control} name='dateOfBirth'
+                          label='Date of Birth' placeholder='yyyy-mm-dd' />
+                        <CustomInput control={form.control} name='ssn'
+                          label='SSN' placeholder='ex: 1234' />
+                      </div>
+                    </>
+                  )
+                }
+                <CustomInput control={form.control} name='email'
+                  label='Email' placeholder='Enter your email' />
+                <CustomInput control={form.control} name='password'
+                  label='Password' placeholder='Enter your password' />
 
-                        </Input>
-                      </FormControl>
+                <div className='flex flex-col gap-4'>
+                  <Button type="submit" className='form-btn' disabled={isLoading}>
+                    {isLoading ? (
+                      <>
+                        <Loader2 size={20} className='animate-spin' /> &nbsp; Loading
+                      </>
+                    ) : type === 'sign-in' ? 'Sign In' : 'Sign Up'}
+                  </Button>
+                </div>
 
-                      <FormMessage className='form-message mt-2'>
+              </form>
+            </Form>
 
-                      </FormMessage>
+            <footer className='flex justify-center gap-1'>
+              <p className='text-14 font-normal text-gray-600'>
+                {type === 'sign-in' ? "Don't have an account?" : "Already have an account?"}
+              </p>
 
-                    </div>
-                  </div>
-                )}
-              />
+              <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className='form-link'>
+                {type === 'sign-in' ? 'Sign Up' : 'Sign In'}
+              </Link>
 
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <div className='form-item'>
-                    <FormLabel className='form-label'>
-                      Password
-                    </FormLabel>
-                    <div className='flex w-full flex-col'>
-                      <FormControl>
-                        <Input placeholder='Enter your password'
-                          className='input-class' type='password' {...field}>
+            </footer></>
 
-                        </Input>
-                      </FormControl>
-
-                      <FormMessage className='form-message mt-2'>
-
-                      </FormMessage>
-
-                    </div>
-                  </div>
-                )}
-              />
-              <Button type="submit">Submit</Button>
-            </form>
-          </Form>
         )}
-      </header></section>
+      </header>
+
+    </section>
   )
 }
 
