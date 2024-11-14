35fd58900b52efb99110b1ed2dd1a8b0cff1a144
tringuyen@motive <95064327+tringuyenCoding@users.noreply.github.com>
2024-11-14 10:25:09 +0700

    View list of cards in myBank page

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,3 +1,4 @@
+import BankCard from '@/components/BankCard';
 import HeaderBox from '@/components/HeaderBox'
 import { getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
@@ -10,12 +11,17 @@ const MyBanks = async () => {
     <section className='flex'>
       <div className='my-banks'>
         <HeaderBox title='Bank Accounts'
-          subtext='Effortlessly manage your banking activities' 
-          />
+          subtext='Effortlessly manage your banking activities'
+        />
         <div className='space-y-4'>
           <h2 className='header-2'>
             Your cards
           </h2>
+          <div className='flex flex-wrap gap-6'>
+            {accounts && accounts.data.map((a: Account) => (
+              <BankCard key={accounts.id} account={a} userName={loggedIn?.firstName} />
+            ))}
+          </div>
         </div>
       </div>
     </section>
--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,11 +2,12 @@ import { formatAmount } from '@/lib/utils'
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
+import Copy from './Copy'
 
 const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
   return (
     <div className='flex flex-col'>
-      <Link href='/' className='bank-card'>
+      <Link href={`/transaction-history/?id=${account.appwriteItemId}`} className='bank-card'>
         <div className='bank-card_content'>
           <div>
             <h1 className='text-16 font-semibold
@@ -30,8 +31,8 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
             </div>
 
             <p className='text-14 font-semibold tracking-[1.1px] text-white'>
-              ●●●● ●●●● ●●●● <span>
-                {1234}
+              ●●●● ●●●● ●●●● <span className='text-16'>
+                {account?.mask}
               </span>
             </p>
           </article>
@@ -61,6 +62,8 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
           className='absolute top-0 left-0'
         />
       </Link>
+
+      {showBalance && (<Copy title={account.sharableId} />)}
     </div>
   )
 }
