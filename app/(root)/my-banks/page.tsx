a4b998c33d9cb8e4c7a2bf9fd6ca46d65f19cf64
tringuyen@motive <95064327+tringuyenCoding@users.noreply.github.com>
2024-11-14 10:06:29 +0700

    Complete header of myBank page

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,8 +1,24 @@
+import HeaderBox from '@/components/HeaderBox'
+import { getAccounts } from '@/lib/actions/bank.actions';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react'
 
-const MyBanks = () => {
+const MyBanks = async () => {
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn.$id });
   return (
-    <div>MyBanks</div>
+    <section className='flex'>
+      <div className='my-banks'>
+        <HeaderBox title='Bank Accounts'
+          subtext='Effortlessly manage your banking activities' 
+          />
+        <div className='space-y-4'>
+          <h2 className='header-2'>
+            Your cards
+          </h2>
+        </div>
+      </div>
+    </section>
   )
 }
 
