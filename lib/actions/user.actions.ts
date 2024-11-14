34f376043b30b2797945ceea6cb6e916bbaa4994
tringuyen@motive <95064327+tringuyenCoding@users.noreply.github.com>
2024-11-14 10:40:13 +0700

    Complete the form of payment transfer page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,10 +6,8 @@ import { ID, Query } from "node-appwrite";
 import { encryptId, extractCustomerIdFromUrl, parseStringify } from "../utils";
 import { CountryCode, ProcessorTokenCreateRequest, ProcessorTokenCreateRequestProcessorEnum, Products } from "plaid";
 import { plaidClient } from "@/lib/plaid";
-import { Processor } from "postcss";
 import { revalidatePath } from "next/cache";
 import { addFundingSource, createDwollaCustomer } from "./dwolla.actions";
-import { Ban } from "lucide-react";
 
 const { APPWRITE_DATABASE_ID: DATABASE_ID,
   APPWRITE_USER_COLLECTION_ID: USER_COLLECTION_ID , APPWRITE_BANK_COLLECTION_ID: BANK_COLLECTION_ID } = process.env;
@@ -246,13 +244,32 @@ export const getBanks = async ({ userId }: getBanksProps) => {
 
 export const getBank = async ({ documentId }: getBankProps) => {
   try {
-    const {database} = await createAdminClient();
+    const { database } = await createAdminClient();
 
     const bank = await database.listDocuments(
       DATABASE_ID!,
       BANK_COLLECTION_ID!,
       [Query.equal('$id', [documentId])]
     );
+
+    return parseStringify(bank.documents[0]);
+  } catch (error) {
+    console.error("An error occurred while getting the banks:", error);
+  }
+}
+
+
+export const getBankByAccountId = async ({ accountId }: getBankByAccountIdProps) => {
+  try {
+    const { database } = await createAdminClient();
+
+    const bank = await database.listDocuments(
+      DATABASE_ID!,
+      BANK_COLLECTION_ID!,
+      [Query.equal('accountId', [accountId])]
+    );
+
+    if (bank.total !== 1) return null;
 
     return parseStringify(bank.documents[0]);
   } catch (error) {
